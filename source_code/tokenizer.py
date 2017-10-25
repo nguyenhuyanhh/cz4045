a9f930c6809559e2e19cde2735cab0363731e18f
iblues93 <oxitachixo@hotmail.com>
2017-10-25 19:55:52 +0800

    Edited URL regex to prevent seeing functions as urls, added in capability for recognising words with _ as tokens

--- source_code/tokenizer.py
+++ source_code/tokenizer.py
@@ -10,9 +10,9 @@ TAG_REG = re.compile(
     r'(</?(?!code)(?:[a-zA-Z]+[1-6]?)\s*(?:\w+?="[\W\w]+?")?\s*>)')
 CODE_REG = re.compile(r'<code>[\W\w]*?</code>')
 URL_REG = re.compile(
-    r'(?:https?://)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+    r'(?:https?://)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)(?!\()')
 EXCEPTION_REG = re.compile(
-    r'((?:\[.*?\])|(?:\{.*?\})|(?:i\.e\.?)|(?:e\.g\.?)|(?:(?:\w.*?\.)?\w+\(.*?\)))')
+    r'((?:\[.*?\])|(?:\{.*?\})|(?:i\.e\.?)|(?:e\.g\.?)|(?:(?:\w[\S]*?\.)?\w+\([\S]*?\))|(?:(?:\w[\S]*?\.)?\w+[\S]*?)|(?:_+[A-z0-9]+(?:_[A-z0-9]*)+))')
 TOK_REG = re.compile(
     r'((?:\d+\.\d+)|(?:\w+)(?:n\'t)|(?:n\'t)|(?:\'s)|(?:[^\s\w]+)|(?:\w+)|(?:\w+\.\w+))')
 
@@ -31,7 +31,7 @@ def tokenize(in_string, tag_reg, code_reg, url_reg, exception_reg, tok_reg):
 
 
 if __name__ == '__main__':
-    test_string = '<p>my string.</p><code>sfdsfdsfds\n\n\n\n\n\n(sdfdsfd)</code> function() obj.func() func(arg) oodp.method(arg) [hello] {world} [{testingdfig}] [e.g.] e.g i.e i.e. http:google.com google.com test.com fdsfg <code> 2nd code</code><a href="sdgdsfdsfds">fdsfsdfdsf</a>'
+    test_string = '<p>my string.</p><code>sfdsfdsfds\n\n\n\n\n\n(sdfdsfd)</code> function() _test _test_test _test_ test_test ash6.sad34sdf 555 obj.func() func(arg) oodp.method(arg) [hello] {world} [{testingdfig}] [e.g.] e.g i.e i.e. http:google.com google.com test.com fdsfg <code> 2nd code</code><a href="sdgdsfdsfds">fdsfsdfdsf</a>'
     test_string_2 = "<blockquote> 3<a and b>5 </a></blockquote> hello \n world <h1></h1>"
     tokenize(test_string, TAG_REG, CODE_REG, URL_REG, EXCEPTION_REG, TOK_REG)
     tokenize(test_string_2, TAG_REG, CODE_REG, URL_REG, EXCEPTION_REG, TOK_REG)
