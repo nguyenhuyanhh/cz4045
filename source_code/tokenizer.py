a40e60d403a6a8d0c83f12324209925454a734f1
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-10-26 13:55:02 +0800

    tokenize_v2 magic

--- source_code/tokenizer.py
+++ source_code/tokenizer.py
@@ -12,11 +12,11 @@ CODE_REG = re.compile(r'<code>[\W\w]*?</code>')
 URL_REG = re.compile(
     r'(?:https?://)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)(?!\()')
 FILE_REG = re.compile(
-    r'((?:(?:/[A-z0-9\_\-\.]+)+)|(?:(?:[A-z]:)?(?:\\[A-z0-9\.]+)+))')
+    r'(?:(?:/[A-z0-9\_\-\.]+)+)|(?:(?:[A-z]:)?(?:\\[A-z0-9\.]+)+)')
 EXCEPTION_REG = re.compile(
-    r'((?:[A-z0-9]+-[A-z0-9]+)|(?:\[.*?\])|(?:\{.*?\})|(?:i\.e\.?)|(?:e\.g\.?)|(?:(?:\w[\S]*?\.)?\w+\([\S]*?\))|(?:(?:\w[\S]*?\.)?\w+[\S]*?)|(?:_+[A-z0-9]+(?:_[A-z0-9]*)+)|(?:\$[A-z][A-z0-9]+))')
+    r'(?:[A-z0-9]+-[A-z0-9]+)|(?:\[.*?\])|(?:\{.*?\})|(?:i\.e\.?)|(?:e\.g\.?)|(?:(?:\w[\S]*?\.)?\w+\([\S]*?\))|(?:(?:\w[\S]*?\.)?\w+[\S]*?)|(?:_+[A-z0-9]+(?:_[A-z0-9]*)+)|(?:\$[A-z][A-z0-9]+)')
 TOK_REG = re.compile(
-    r'((?:\d+\.\d+)|(?:\w+)(?:n\'t)|(?:n\'t)|(?:\'s)|(?:[^\s\w]+)|(?:\w+)|(?:\w+\.\w+))')
+    r'(?:\d+\.\d+)|(?:\w+)(?:n\'t)|(?:n\'t)|(?:\'s)|(?:[^\s\w]+)|(?:\w+)|(?:\w+\.\w+)')
 
 
 def tokenize(in_string):
@@ -32,8 +32,61 @@ def tokenize(in_string):
     exception_tokens = EXCEPTION_REG.findall(no_url)    # take out exceptions
     no_exception = EXCEPTION_REG.sub(" ", no_url)       # remove exceptions
     word_tokens = TOK_REG.findall(no_exception)         # tokenize! :)
-    print(code_snippets + url_tokens + file_token +
-          exception_tokens + word_tokens)
+
+    return (code_snippets + url_tokens + file_token +
+            exception_tokens + word_tokens)
+
+
+def tokenize_op(in_string, reg_obj, pos_list=None, tokens=None):
+    """An atom tokenization operation, using a single regex.
+
+    Arguments:
+        in_string: str - the source string to search for tokens
+        reg_obj: SRE_Pattern - compiled regex
+        pos_list: list(list(int,int)) - list of start/end indexes to search
+        tokens: dict - tokens dictionary
+    Returns:
+        ret_pos_list: list of start/end indexes of non-matches
+        tokens - tokens dictionary
+    """
+    # init returns and None-type arguments
+    ret_pos_list = []
+    if not tokens:
+        tokens = {}
+    if not pos_list:
+        pos_list = [[0, len(in_string)]]
+
+    for index in pos_list:
+        # init start and end index to search
+        start = index[0]
+        end = index[1]
+        non_match_bounds = [start]
+        # only search for matches from start to end
+        for match in reg_obj.finditer(in_string[start:end]):
+            tokens[start + match.start()] = match.group()
+            non_match_bounds += [start + match.start(), start + match.end()]
+        non_match_bounds += [end]
+        # append non-match bounds to return list
+        ret_pos_list += [[non_match_bounds[2 * i], non_match_bounds[2 * i + 1]] for i in range(
+            len(non_match_bounds) // 2) if non_match_bounds[2 * i] != non_match_bounds[2 * i + 1]]
+    return ret_pos_list, tokens
+
+
+def tokenize_v2(in_string):
+    """A better tokenizer that returns tokens in-order."""
+    # remove html tags
+    in_string = TAG_REG.sub(' ', in_string)
+
+    # init
+    tokens = None
+    ret_pos_list = None
+    regexes = [CODE_REG, FILE_REG, URL_REG, EXCEPTION_REG, TOK_REG]
+
+    # operations, one-by-one in that order
+    for reg in regexes:
+        ret_pos_list, tokens = tokenize_op(
+            in_string, reg, ret_pos_list, tokens)
+    return [tokens[k] for k in sorted(tokens)]
 
 
 def evaluate(tokens, truth):
@@ -76,9 +129,9 @@ def evaluate(tokens, truth):
 if __name__ == '__main__':
     test_string = '<p>my string.</p><code>sfdsfdsfds\n\n\n\n\n\n(sdfdsfd)</code> function() length-2 _test /nfs/an/disks/jj/home/dir/file.txt /dev/test/file.txt _test_test $1.00 _test_ test_test $interpolateProvider ash6.sad34sdf 555 obj.func() func(arg) oodp.method(arg) [hello] {world} [{testingdfig}] [e.g.] e.g i.e i.e. http:google.com google.com test.com fdsfg <code> 2nd code</code><a href="sdgdsfdsfds">fdsfsdfdsf</a>'
     test_string_2 = "<blockquote> 3<a and b>5 </a></blockquote> /public \\file\\name\\test.txt /nfs/an/disks/jj/home/dir/file.txt C:\\Users\\Deon\\SchoolWork hello \n world <h1></h1>"
-    tokenize(test_string)
-    tokenize(test_string_2)
-    
+    assert set(tokenize(test_string)) == set(tokenize_v2(test_string))
+    print(tokenize_v2(test_string))
+
     tok = ['1', '3', 'a', 'hdytuiwegduig',
            '<code></code>', 'hello', 'x', 'b']
     tru = ['1', '3', 'ahdytuiwegduig', '<code></code>', 'hello', 'a']
