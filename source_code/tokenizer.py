6edd5481ee5184c0196e46e604a8a786ba740592
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-10-26 11:49:08 +0800

    remove regexes from tokenize call

--- source_code/tokenizer.py
+++ source_code/tokenizer.py
@@ -19,23 +19,25 @@ TOK_REG = re.compile(
     r'((?:\d+\.\d+)|(?:\w+)(?:n\'t)|(?:n\'t)|(?:\'s)|(?:[^\s\w]+)|(?:\w+)|(?:\w+\.\w+))')
 
 
-def tokenize(in_string, tag_reg, code_reg, url_reg, file_reg, exception_reg, tok_reg):
+def tokenize(in_string):
     """Tokenize a string using our rules."""
-    no_tags = tag_reg.sub(' ', in_string)               # remove html tags
-    code_snippets = code_reg.findall(no_tags)           # take out the code snippets
-    no_code = code_reg.sub(' ', no_tags)                # remove code snippet
-    file_token = file_reg.findall(no_code)              # extract paths
-    no_file = file_reg.sub(' ',no_code)                 # remove paths
-    url_tokens = url_reg.findall(no_file)               # take out urls
-    no_url = url_reg.sub(' ', no_file)                  # remove urls
-    exception_tokens = exception_reg.findall(no_url)    # take out exceptions
-    no_exception = exception_reg.sub(" ", no_url)       # remove exceptions
-    word_tokens = tok_reg.findall(no_exception)         # tokenize! :)
-    print(code_snippets + url_tokens + file_token + exception_tokens + word_tokens)
+    no_tags = TAG_REG.sub(' ', in_string)               # remove html tags
+    # take out the code snippets
+    code_snippets = CODE_REG.findall(no_tags)
+    no_code = CODE_REG.sub(' ', no_tags)                # remove code snippet
+    file_token = FILE_REG.findall(no_code)              # extract paths
+    no_file = FILE_REG.sub(' ', no_code)                 # remove paths
+    url_tokens = URL_REG.findall(no_file)               # take out urls
+    no_url = URL_REG.sub(' ', no_file)                  # remove urls
+    exception_tokens = EXCEPTION_REG.findall(no_url)    # take out exceptions
+    no_exception = EXCEPTION_REG.sub(" ", no_url)       # remove exceptions
+    word_tokens = TOK_REG.findall(no_exception)         # tokenize! :)
+    print(code_snippets + url_tokens + file_token +
+          exception_tokens + word_tokens)
 
 
 if __name__ == '__main__':
     test_string = '<p>my string.</p><code>sfdsfdsfds\n\n\n\n\n\n(sdfdsfd)</code> function() length-2 _test /nfs/an/disks/jj/home/dir/file.txt /dev/test/file.txt _test_test $1.00 _test_ test_test $interpolateProvider ash6.sad34sdf 555 obj.func() func(arg) oodp.method(arg) [hello] {world} [{testingdfig}] [e.g.] e.g i.e i.e. http:google.com google.com test.com fdsfg <code> 2nd code</code><a href="sdgdsfdsfds">fdsfsdfdsf</a>'
     test_string_2 = "<blockquote> 3<a and b>5 </a></blockquote> /public \\file\\name\\test.txt /nfs/an/disks/jj/home/dir/file.txt C:\\Users\\Deon\\SchoolWork hello \n world <h1></h1>"
-    tokenize(test_string, TAG_REG, CODE_REG, URL_REG, FILE_REG, EXCEPTION_REG, TOK_REG)
-    tokenize(test_string_2, TAG_REG, CODE_REG, URL_REG, FILE_REG, EXCEPTION_REG, TOK_REG)
+    tokenize(test_string)
+    tokenize(test_string_2)
