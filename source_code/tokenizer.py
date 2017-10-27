a3e080f0c538a5b6d1b9a8db2bcc2c41391ac43d
iblues93 <oxitachixo@hotmail.com>
2017-10-27 19:49:08 +0800

    before merging url and file

--- source_code/tokenizer.py
+++ source_code/tokenizer.py
@@ -12,11 +12,11 @@ CODE_REG = re.compile(r'<code>[\W\w]*?</code>')
 URL_REG = re.compile(
     r'(?:https?://)?[-@:%.\+~#=\w]{2,256}\.[a-z]{2,6}\b(?:[-@:%\+\w.~#?&/=]*)(?!\()')
 #FILE_REG = re.compile(
-  # r'(?:/(?:[\w][ \w\-]*/)+[\w\-\.]+)|(?:(?:[A-z]:)?(?:\\[ A-z0-9\.]+)+)')
+   # r'(?:(?:[A-z]:)?(?:\\[ A-z0-9\.]+)+)')
 #FILE_REG = re.compile(
     #r'(?:(/[\w.]))|(?:[a-zA-Z]:\\(((?![<>:"/\\|?*]).)+((?<![ .])\\)?)*)')
 FILE_REG = re.compile(
-    r'(?:(?:\.?\.)?(/(?:(?:\.\.)|[\w][\w\.-_]*))+)|(?:\.{2,3})|(?:[a-zA-Z]:\\(?:(?:(?![<>:"/\\|?*]).)+(?:(?<![ .])\\)?)*)')
+    r'(?:(?:\.?\.)?((?<!:/)/(?:(?:\.\.)|[\w][\w\.-_]*))+)|(?:\.{2,3})|(?:[a-zA-Z]:\\(?:(?:(?![<>:"/\\|?*]).)+(?:(?<![ .])\\)?)*)')
 EXCEPTION_REG = re.compile(
     r'(?:[A-z0-9]+-[A-z0-9]+)|(?:\[.*?\])|(?:\{.*?\})|(?:i\.e\.?)|(?:e\.g\.?)|(?:(?:\w[\S]*?\.)?\w+\([\S]*?\))|(?:[A-z][A-z0-9]*\.[A-z][A-z0-9]*)|(?:_+[A-z0-9]+(?:_[A-z0-9]*)+)|(?:\$[A-z][A-z0-9]+)')
 TOK_REG = re.compile(
@@ -137,7 +137,7 @@ def evaluate(tokens, truth):
 
 if __name__ == '__main__':
     test_string = '<p>my string.</p><code>sfdsfdsfds\n\n\n\n\n\n(sdfdsfd)</code> function() length-2 _test /nfs/an/disks/jj/home/dir/file.txt /dev/test/file.txt _test_test $1.00 _test_ test_test $interpolateProvider ash6.sad34sdf 555 obj.func() func(arg) oodp.method(arg) [hello] {world} [{testingdfig}] [e.g.] e.g i.e i.e. http:google.com google.com test.com fdsfg <code> 2nd code</code><a href="sdgdsfdsfds">fdsfsdfdsf</a>'
-    test_string_2 = "\"Hello, world!\" cd . ... ../ ../../ ./helloworld/test /dev/test.here/filetest.txt ../../.. ../../here/text.txt ..  C:\\Users\\Program Files (x86)\\SchoolWork"
+    test_string_2 = "http://google.com https://googl.com https://google.com/query#div?q=hello&a=test http://google.com/query#div?q=hello&a=test google.com/query#div?q=hello&a=test"
     assert set(tokenize(test_string)) == set(tokenize_v2(test_string))
     #print(tokenize_v2(test_string))
     print(tokenize_v2(test_string_2))
