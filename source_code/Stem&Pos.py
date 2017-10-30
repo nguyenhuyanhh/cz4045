239e7b21ff72cc22be3d713017c650203b531a7a
yokechew <ohyc93@gmail.com>
2017-10-30 18:31:57 +0800

    Final Stem&Pos

--- source_code/Stem&Pos.py
+++ source_code/Stem&Pos.py
@@ -1,8 +1,10 @@
 import nltk as nltk
 import pandas as pd
 import re as re
+import random as random
 import itertools
 from collections import Counter
+
 ##read data
 data = pd.read_csv('QueryResults.csv')
 
@@ -10,32 +12,42 @@ data = pd.read_csv('QueryResults.csv')
 sent_tokenized_data = [nltk.sent_tokenize(re.sub(r"<.*?>", r"", i)) for i in data.iloc[:,3]]
 print(sent_tokenized_data)
 
-##word tokenize each row
+##word tokenize each row  without removing punctuations
 tokenized_data = [nltk.word_tokenize(i[0]) for i in sent_tokenized_data]
-#tokenized_data = [nltk.word_tokenize(re.sub(r"<.*?>|\W", r" ", i)) for i in data.iloc[:,0]]
 print(tokenized_data)
-
-##regex tokenizer
-#regexstm = [nltk.regexp_tokenize(i,r"<code>[\W\w]*?<\/code>|(\d+\.\d+)|(\w+)(?=n't)|(n't)|('s)|([^\s\w]+)|(\w+)|(\w+\.\w+)",gaps=True) for i in data.iloc[:,3]]
-#print(regexstm)
 
 ##POS tagging
 pos_tagged = nltk.pos_tag_sents(tokenized_data)
-print(pos_tagged)
+print(random.sample(pos_tagged,10))
+
+##word tokenize each row
+tokenized_data = [[re.sub(r"[^\w\s]", r"",j) for j in nltk.word_tokenize(i[0])] for i in sent_tokenized_data]
+print(tokenized_data)
 
 ##remove stopwords
 no_stopwords = [[word for word in sent if word not in nltk.corpus.stopwords.words('english')] for sent in tokenized_data]
 
 ##Stemming data
-#stemmed_data  = [[nltk.stem.PorterStemmer().stem(word) for word in sent if word not in nltk.corpus.stopwords.words('english')] for sent in tokenized_data]
 stemmed_data = [[nltk.stem.PorterStemmer().stem(word) for word in sent] for sent in no_stopwords]
 
-#count words before stem
+##count words before stem
 temporary = list(itertools.chain.from_iterable(no_stopwords))
 counted = Counter(temporary)
-print(counted.most_common(40))
+print(counted.most_common(21))
 
-#count words after stem
+##count words after stem
 temporary = list(itertools.chain.from_iterable(stemmed_data))
 counted = Counter(temporary)
-print(counted.most_common(40))
+all = counted.most_common(21)
+print(all)
+
+##find original words that stemmed to "all"
+original_words = []
+for i in range(21):
+    original_words.append([])
+    for sent in no_stopwords:
+        for word in sent:
+            if nltk.stem.PorterStemmer().stem(word) == all[i][0]:
+                if word not in original_words[i]:
+                    original_words[i].append(word)
+print(original_words)
